1707eb66c0c7cae90d13f9b0d1d2e30f59d882d8
edipojuan <edipojs@gmail.com>
2019-06-12 14:52:19 -0400

    updated map.page.ts

--- src/app/pages/map/map.page.ts
+++ src/app/pages/map/map.page.ts
@@ -18,15 +18,18 @@ export class MapPage implements OnInit {
   }
 
   loadMap() {
-    console.log('Google Maps API version: ' + google.maps.version);
+    console.log(`Google Maps API version: ${google.maps.version}`);
 
-    const latLng = new google.maps.LatLng(-15.568601, -56.094145);
+    const latLng = new google.maps.LatLng(-15.553609, -56.096618);
+
+    // -15.553609, -56.096618
 
     const mapOptions = {
       center: latLng,
-      zoom: 18,
-      // mapTypeId: google.maps.MapTypeId.ROADMAP,
-      mapTypeId: 'satellite',
+      // zoom: 13,
+      zoom: 13,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      // mapTypeId: 'satellite',
       // mapTypeId: 'terrain',
       heading: 90,
       tilt: 45
@@ -34,6 +37,42 @@ export class MapPage implements OnInit {
 
     this.map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
+    this.setCurrentPosition();
+
+    this.addMarkers();
+
+    // this.autoRotate();
+  }
+
+  detectBrowser() {
+    const useragent = navigator.userAgent;
+    const mapdiv = document.getElementById('map');
+
+    if (
+      useragent.indexOf('iPhone') !== -1 ||
+      useragent.indexOf('Android') !== -1
+    ) {
+      mapdiv.style.width = '100%';
+      mapdiv.style.height = '100%';
+    } else {
+      mapdiv.style.width = '600px';
+      mapdiv.style.height = '800px';
+    }
+  }
+
+  autoRotate() {
+    let heading = 90;
+
+    // Determine if we're showing aerial imagery.
+    if (this.map.getTilt() !== 0) {
+      window.setInterval(() => {
+        heading = heading === 0 ? 90 : 0; // this.map.getHeading() || 0;
+        this.map.setHeading(heading + 90);
+      }, 3000);
+    }
+  }
+
+  addMarkers() {
     const positions = [
       new google.maps.LatLng(-15.568601, -56.094145) /* despraiado */,
       new google.maps.LatLng(-15.568642, -56.057323) /* moradaDoOuro */
@@ -44,7 +83,9 @@ export class MapPage implements OnInit {
     const markers = positions.map((position, i) => {
       return new google.maps.Marker({ position, map, label: `Test ${i}` });
     });
+  }
 
+  setCurrentPosition() {
     const infoWindow = new google.maps.InfoWindow();
 
     if (navigator.geolocation) {
@@ -65,16 +106,6 @@ export class MapPage implements OnInit {
     } else {
       this.handleLocationError(false, infoWindow, this.map.getCenter());
     }
-
-    // this.autoRotate();
-    // this.map.setHeading(0 + 90);
-    let heading = 90;
-
-    window.setInterval((x) => {
-      console.log(heading);
-      heading = heading === 0 ? 90 : 0;
-      this.map.setHeading(heading);
-    }, 2000);
   }
 
   handleLocationError(browserHasGeolocation, infoWindow, pos) {
@@ -85,33 +116,5 @@ export class MapPage implements OnInit {
         : 'Error: Your browser doesn\'t support geolocation.'
     );
     infoWindow.open(this.map);
-  }
-
-  detectBrowser() {
-    const useragent = navigator.userAgent;
-    const mapdiv = document.getElementById('map');
-
-    if (
-      useragent.indexOf('iPhone') !== -1 ||
-      useragent.indexOf('Android') !== -1
-    ) {
-      mapdiv.style.width = '100%';
-      mapdiv.style.height = '100%';
-    } else {
-      mapdiv.style.width = '600px';
-      mapdiv.style.height = '800px';
-    }
-  }
-
-  rotate90() {
-    const heading = 0; // this.map.getHeading() || 0;
-    this.map.setHeading(heading + 90);
-  }
-
-  autoRotate() {
-    // Determine if we're showing aerial imagery.
-    if (this.map.getTilt() !== 0) {
-      window.setInterval(this.rotate90, 3000);
-    }
   }
 }
